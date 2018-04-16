2266a60fa5d3d59b4b4557396ea4160fff3c212e
HeltonAlveseletric <38401598+HeltonAlveseletric@users.noreply.github.com>
2018-04-15 22:09:23 -0300

    ProjetoFinal

--- genericsobel/sobelf/sobelf.sim/sim_1/behav/elaborate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=D:\\Vivado\\Vivado\\2016.4\\bin
-call %xv_path%/xelab  -wto 8e226c8c407a48c19f82860e52934f87 -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L secureip --snapshot tb_sobelf_behav xil_defaultlib.tb_sobelf -log elaborate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
